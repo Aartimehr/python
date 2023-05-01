675369f83daede209d2df9c3d437f6b757e99ba1
Abecedarian <106958470+Aartimehr@users.noreply.github.com>
2023-05-01 22:27:03 +0530

    Create file.py

---
+++ file.py
@@ -0,0 +1,19 @@
+a = "hello python"
+b = "my name is aarti"
+c = "i am learning python"
+
+
+#slicing
+print(a[5:8:1])
+print(c[5:12:1])
+
+
+#operation
+print(a+b)
+print(c*5)
+
+
+#functions
+#len()
+print(" print length of a: ", end="")
+print(a.find('a'),end=" ")
